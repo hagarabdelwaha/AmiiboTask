3962b1a4586834a081d75f1bd2ac26795f1141fb
hagarabdelwaha <hagarabdelwahab@outlook.com>
2019-01-07 15:42:40 +0200

    get amiibos data

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,28 +1,94 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Amiibo from './components/amiibo';
+import { connect } from 'react-redux';
+import { getAmiiboCards } from './actions/getamiiboaction';
+import { getTotalCardsNumber } from './actions/gettotalaction'
+import axios from 'axios';
+
 
 class App extends Component {
+
+
+  handleChange=(e)=>
+  {
+    if(e.target.value !== '')
+    {
+      this.search(e.target.value);
+    }else
+    {
+      this.getAmiiboCards();
+    }
+    
+  }
+
+  getAmiiboCards=()=>
+  {
+    axios.get('http://localhost:8080/amiibo').then(res=>{
+        this.props.getAmiiboCards(res.data.data.docs);
+        this.props.getTotalCardsNumber(res.data.data.total);
+        
+      }).catch((err)=>{
+        console.log('ERRROR',err);
+      });
+  }
+  
+
+  search=(search)=>
+  {
+    axios.get('http://localhost:8080/amiibo/search?search='+search).then(res=>{     
+         this.props.getAmiiboCards(res.data.data.docs);
+         this.props.getTotalCardsNumber(res.data.data.total)
+      }).catch((err)=>{
+        console.log('ERRROR',err);
+      });
+  }
+
+  componentDidMount(){
+    this.getAmiiboCards();
+  }  
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <div className="topnav">
+          <h3>AMIIBOS</h3>
+            <div className="search-container">
+            <input type="text" placeholder="Search.." name="search" onChange={this.handleChange}/>
+            </div>
+          </div>
         </header>
+        <div className="sidenav">
+           User Data
+           <h3>Mario</h3>
+           <h4>Total Cards : {this.props.total}</h4>
+           <h4>Visible Cards :{this.props.amiibos.length}</h4>
+        </div>
+        { 
+          this.props.amiibos.length === 0 ? <h4>No cards match search </h4> : 
+          this.props.amiibos.map(amiibo =>{
+           return <Amiibo key={amiibo._id} amiibo={amiibo}/>
+          })
+        }
+
+        
       </div>
     );
   }
 }
 
-export default App;
+const mapStateToProps=(state)=>{
+ return {
+   amiibos: state.amiibos,
+   total: state.total
+ }
+};
+
+
+const mapDispatchToProps = (dispatch) => ({
+  getAmiiboCards: (cards) => dispatch(getAmiiboCards(cards)),
+  getTotalCardsNumber:(num)=>dispatch(getTotalCardsNumber(num))
+});
+
+export default connect(mapStateToProps,mapDispatchToProps)(App);
---
+++ reactapp/src/components/amiibo.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+
+class Amiibo extends Component {
+  render() {
+    return (
+      <div className='card'>
+        <img src={this.props.amiibo.image} width='40%'></img>
+        {/* <p>{this.props.amiibo.name}</p> */}
+        {/* <p>{this.props.amiibo.amiiboSeries}</p> */}
+        <p>{this.props.amiibo.character}</p>
+        {/* <p>{this.props.amiibo.gameSeries}</p> */}
+        
+        
+      </div>
+    );
+  }
+}
+
+export default Amiibo;
