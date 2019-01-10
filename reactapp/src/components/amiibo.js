ff7db5cc4f11a6f6a0ea446661d0cb7d0b33dd85
hagarabdelwaha <hagarabdelwahab@outlook.com>
2019-01-10 13:15:42 +0200

    display amiibo name

--- reactapp/src/components/amiibo.js
+++ reactapp/src/components/amiibo.js
@@ -7,6 +7,7 @@ class Amiibo extends Component {
       <div className='card'>
         <img src={this.props.amiibo.image} width='40%'></img>
         <p>{this.props.amiibo.name}</p>
+        
         {/* <p>{this.props.amiibo.amiiboSeries}</p> */}
         {/* <p>{this.props.amiibo.character}</p> */}
         {/* <p>{this.props.amiibo.gameSeries}</p> */}
