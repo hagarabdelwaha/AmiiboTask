9507cde4e2f57fd5f8ffc50b9fa15448f577ba17
hagarabdelwaha <hagarabdelwahab@outlook.com>
2019-01-11 15:44:24 +0200

    serve react app from node

--- nodeApp/app.js
+++ nodeApp/app.js
@@ -27,11 +27,11 @@ var corsOptions = {
 }
 app.use(cors(corsOptions));
 
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, 'reactapp/build')));
 // app.use('/', indexRouter);
 
 app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, 'reactapp/build', 'index.html'));
 });
 
 app.use('/users', usersRouter);
